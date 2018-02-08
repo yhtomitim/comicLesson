8fb07ec8ccf74150c0d7cb477c9932d6df0e4628
yhtomitim <yhtomitim@me.com>
2018-02-08 09:44:05 -0700

    added ajax to xkcd

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ $(document).ready(function(){
     $.get('https://galvanize-cors.herokuapp.com/https://xkcd.com/info.0.json')
       .then(function(data){
 
+        $("h1").text(data.title);
+        $("p").text(data.alt);
+        $("img").attr("src",data.img);
+      var safeTitle = $("<p></p>").text(data.safe_title);
+        $("h1").after(safeTitle);
+      var comicNumber = $("<p></p>").text("Comic Number " + data.num);
+        $("img").before(comicNumber);
+
       })
 })
